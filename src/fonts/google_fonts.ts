d79a2f10c80407ab8888255926051a9c1b915bf8
別府大地 <iamyouarepocky@icloud.com>
2023-12-01 14:09:52 +0900

    feat: googlefontの実装、KiwiMaruフォントを追加

---
+++ src/fonts/google_fonts.ts
@@ -0,0 +1,8 @@
+import "@/app/globals.css";
+import { Kiwi_Maru } from "next/font/google";
+
+export const KiwiMaru = Kiwi_Maru({
+  weight: "400",
+  subsets: ["latin"],
+  variable: "--font-KiwiMaru",
+});
