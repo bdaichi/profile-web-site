896a140e8264ceeccc41e57d1d4b4ef3501a6259
別府大地 <iamyouarepocky@icloud.com>
2023-12-01 18:04:20 +0900

    fix: 画像サイズについてのバグが出ていたので修正

--- src/components/home_page/profile_detail_tile.tsx
+++ src/components/home_page/profile_detail_tile.tsx
@@ -28,7 +28,7 @@ export default function ProfileDetailTile(props: Props) {
   return (
     <div
       className={`${KiwiMaruFont.className} flex justify-center w-72 mt-28 rounded-md relative shadow-2xl`}
-      style={{ height: 396.5 }}
+      style={{ height: 400 }}
     >
       <div id="back_ground">
         <Image
@@ -37,9 +37,10 @@ export default function ProfileDetailTile(props: Props) {
           } `}
           src="/douga_gui.jpg" //すいかにかぶりついている画像
           alt="スイカを動画食い *動画食いとは手を使わずに口のみで食らうこと"
-          width={300}
-          height={450}
-          style={{ opacity: 0.3 }}
+          width="0"
+          height="0"
+          sizes="100vw"
+          style={{ opacity: 0.3, height: 400, width: 300 }}
         />
       </div>
       <div
@@ -54,11 +55,12 @@ export default function ProfileDetailTile(props: Props) {
           className={`z-0 h-32 w-32 my-12 rounded-full shadow-l md:mt-24 ${
             isPc ? "mt-0" : "mt-14"
           } `}
-          src="/hage.jpeg" //すいかにかぶりついている画像
-          alt="スイカを動画食い *動画食いとは手を使わずに口のみで食らうこと"
-          width={200}
-          height={200}
-          style={{ opacity: 0.9 }}
+          src="/hage.jpeg"
+          alt="ハゲ"
+          width="0"
+          height="0"
+          sizes="100vw"
+          style={{ opacity: 0.9, width: 170, height: 170 }}
         />
         <p className="flex justify-center text-3xl">名前　別府大地</p>
         <div className="flex flex-row flex-wrap justify-center items-center ml-4 my-2">
